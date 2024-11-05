9a97fce7904f1c83116a41d2ba840ace6ab3054d
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-11-05 23:16:46 +0200

    css and js <> hero-text updated(1)

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,29 @@
                 { opacity: 0, y: -20 },
                 { opacity: 1, y: 0, duration: 1.5, delay: 1.5 }
             );
-            
+
             gsap.fromTo(
                 ".media ul li",
                 { opacity: 0, x: 20 },
                 { opacity: 1, x: 0, duration: 1, delay: 3, stagger: 0.2, ease: "power2.out" } 
             );
+            gsap.from(".text h1 .text-bar", {
+                duration: 1.5,
+                delay: 1.6,
+                y: "100%",
+                ease: "expo.inOut"
+            });
+            gsap.from(".text p .text-bar", {
+                duration: 1.5,
+                delay: 2,
+                y: "100%",
+                ease: "expo.inOut"
+            });
+            gsap.to(".bg", {
+                duration: 2,
+                width: "100%",
+                backgroundColor: "black",
+                ease: "expo.inOut"
+            });
+
         });
