1052da08e6b36cc8b2070cb3f195471abd15e139
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-11-05 22:55:59 +0200

    css <> media links updated(1)

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
-gsap.fromTo("nav ul", { opacity: 0, y: -20 }, { opacity: 1, y: 0, duration: 1, delay: 0.5 });
-
+   document.addEventListener("DOMContentLoaded", function() {
+            gsap.fromTo(
+                "nav ul",
+                { opacity: 0, y: -20 },
+                { opacity: 1, y: 0, duration: 1.5, delay: 1.5 }
+            );
+            
+            gsap.fromTo(
+                ".media ul li",
+                { opacity: 0, x: 20 },
+                { opacity: 1, x: 0, duration: 1, delay: 3, stagger: 0.2, ease: "power2.out" } 
+            );
+        });
